b8352dcf67a0ea1799f0876777f646073661ba24
emmegray <lunaienuso@libero.it>
2022-04-12 15:46:09 +0200

    Finished JSnack 1

--- JSnack 1/script.js
+++ JSnack 1/script.js
@@ -1,3 +1,15 @@
 // JSnack 1
-// 1 - L'utente inserisce due parole in successione con due prompt
+// 1 - L'utente inserisce due numeri in successione con due prompt
 // 2 - Il software stampa il maggiore
+
+let primiDatiUtente;
+let secondiDatiUtente;
+
+primiDatiUtente = prompt('Inserisci un numero:');
+secondiDatiUtente = prompt("Inserisci un'altro numero:")
+
+if (primiDatiUtente > secondiDatiUtente) {
+    document.getElementById("dati").textContent = primiDatiUtente
+} else {
+    document.getElementById("dati").textContent = secondiDatiUtente
+}
