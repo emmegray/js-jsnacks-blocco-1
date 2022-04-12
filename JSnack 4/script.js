78ee000c262cd85bffc50e43614a21e53ea0ad7f
emmegray <lunaienuso@libero.it>
2022-04-12 17:40:12 +0200

    Added JSnack 4 folder

---
+++ JSnack 4/script.js
@@ -0,0 +1,6 @@
+// JSnack 4
+// 1 - Creare array con elenco nomi invitati alla festa del grande Gatsby
+// 2 - Chiedere all'utente nome e cognome
+// 3 - Controllare se nome e cognome dell'utente è presente nella lista e comunicarglielo
+// 4 - Comunicare all'utente se può partecipare
+
