ad7c93e0d9d0acfc6b80ddfcff4651e50992bbc6
emmegray <lunaienuso@libero.it>
2022-04-12 16:21:45 +0200

    Finished JSnack 2

--- JSnack 2/script.js
+++ JSnack 2/script.js
@@ -1,3 +1,17 @@
 // JSnack 2
 // 1 - L'utente inserisce due parole in successione con due prompt
 // 2 - Il software stampa prima la parola più corta, poi la parola più lunga
+
+let primiDatiUtente;
+let secondiDatiUtente;
+
+primiDatiUtente = prompt("Inserisci una parola:");
+secondiDatiUtente = prompt("Inserisci un'altra parola");
+
+if (primiDatiUtente.length > secondiDatiUtente.length) {
+    document.getElementById("parolaLunga").textContent = primiDatiUtente
+    document.getElementById("parolaCorta").textContent = secondiDatiUtente
+} else {
+    document.getElementById("parolaLunga").textContent = secondiDatiUtente
+    document.getElementById("parolaCorta").textContent = primiDatiUtente
+}
