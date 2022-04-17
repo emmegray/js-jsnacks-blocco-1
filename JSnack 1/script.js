3a641f3a90c22fce9a8774ca91fa4bfceb5c88b2
Manuela Ienuso <lunaienuso@libero.it>
2022-04-17 15:04:25 +0200

    Fixed JSnack 1

--- JSnack 1/script.js
+++ JSnack 1/script.js
@@ -5,11 +5,23 @@
 let primiDatiUtente;
 let secondiDatiUtente;
 
-primiDatiUtente = prompt('Inserisci un numero:');
-secondiDatiUtente = prompt("Inserisci un'altro numero:")
+primiDatiUtente = inserisciNumeroValido("Inserisci primo numero", "Numero non valido");
+secondiDatiUtente = inserisciNumeroValido("Inserisci secondo numero", "Numero non valido");
 
 if (primiDatiUtente > secondiDatiUtente) {
     document.getElementById("dati").textContent = primiDatiUtente
 } else {
     document.getElementById("dati").textContent = secondiDatiUtente
+}
+
+function inserisciNumeroValido(domanda, messaggioErrore) {
+    let datiUtente = prompt(domanda);
+    let nonUnNumero = Number.isNaN(Number(datiUtente))
+
+    while (nonUnNumero) {
+        alert(messaggioErrore);
+        datiUtente = prompt(domanda);
+    }
+    
+    return Number(datiUtente);
 }
