95f30b8077796712f122ce553bdb3709943d997e
Manuela Ienuso <lunaienuso@libero.it>
2022-04-17 15:27:18 +0200

    Finished JSnack 4

--- JSnack 4/script.js
+++ JSnack 4/script.js
@@ -4,3 +4,15 @@
 // 3 - Controllare se nome e cognome dell'utente è presente nella lista e comunicarglielo
 // 4 - Comunicare all'utente se può partecipare
 
+const invitati = ["Marco", "Fabio", "Coso",];
+const datiUtente = prompt("Inserisci il tuo nome?");
+
+let risultato = invitati.find(function(value){
+  return value === datiUtente  
+})
+
+if (risultato === datiUtente) {
+  alert("Puoi partecipare!");
+} else {
+  alert("Non sei invitato >:(")
+}
